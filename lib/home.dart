d6cb9b5400bff086bc4490b6674260e7b7add67b
Emele-Onu Favour <emeleonufavour15@gmail.com>
2023-03-23 12:54:02 +0100

    fixed bug in tflite

--- lib/home.dart
+++ lib/home.dart
@@ -7,6 +7,8 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
